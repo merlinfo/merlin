d18d8664fdd31a9a05eeb8a8bd9b66b0f5ab441a
freyja <mrender2005@gmail.com>
2021-08-04 00:42:36 -0700

    mutiple arguments

--- src/main.rs
+++ src/main.rs
@@ -21,15 +21,16 @@ fn main() {
 			.help("Silence errors"))
 		.arg(Arg::with_name("NOTATION")
 			.index(1)
+			.multiple(true)
 			.help("Merlin notation to evaluate before entering the shell"))
 		.get_matches();
 
 	let mut p = plane::Plane::new(!merlin_args.is_present("no-errors"));
 	
-	// parse the first argument as MN
+	// parse the first argument(s) as MN
 
-	if let Some(n) = merlin_args.value_of("NOTATION") {
-		p.parse_line(n);
+	if let Some(n) = merlin_args.values_of("NOTATION") {
+		p.parse_line(&n.collect::<Vec<&str>>().join(" "));
 	}
 
 	// start the shell
